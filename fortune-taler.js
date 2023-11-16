a56a3e57c54ce42f75a8890487aac87b27a1496f
Pierre R <pierre.reltien@gmail.com>
2023-11-16 17:23:54 +0100

    added an object and a function

--- fortune-taler.js
+++ fortune-taler.js
@@ -1,10 +1,16 @@
-// Fortune taler script
+// Fortune taler script that tells you how much money you will earn in the future.
 
-const fortune = Math.floor(Math.random() * 1000000);
-const coinFace = ['head', 'tail'];
-const time = ['one month', '1 year', '10 years'];
+const fortuneTalerObject = {
+    fortune: Math.floor(Math.random() * 1000000),
+    coinFace: ['head', 'tail'],
+    time: ['one month', '1 year', '10 years']
+}
 
 
-console.log(`Hello my dear friend. Toss a coin. 
-If the coin face is ${coinFace[Math.floor(Math.random() * 2)]}, 
-then you will earn ${fortune} dollars within ${time[Math.floor(Math.random() * 3)]}.`);
+const fortuneTaler = () => {
+    console.log(`Hello my dear friend. Toss a coin. 
+If the coin face is ${fortuneTalerObject.coinFace[Math.floor(Math.random() * 2)]}, 
+then you will earn ${fortuneTalerObject.fortune} dollars within ${fortuneTalerObject.time[Math.floor(Math.random() * 3)]}.`);
+}
+
+fortuneTaler();
